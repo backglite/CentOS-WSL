66767046f9a47e1af7ce0165303d39c0d7778c6f
Jindrich Hrabal <jindrich.hrabal@eleveo.com>
2022-03-02 10:13:52 +0100

    Update to newer release of CentOS 7

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@
 # Environment variables for the CentOS cloud image
 ARCH="x86_64"
 OS_VER="7"
-ROOTFS_VER="2009"
+ROOTFS_VER="2111"
 ROOTFS_FN="CentOS-${OS_VER}-${ARCH}-GenericCloud-${ROOTFS_VER}.qcow2"
 ROOTFS_URL="http://cloud.centos.org/centos/${OS_VER}/images/${ROOTFS_FN}"
 
 # Environment variables for Yuk7's wsldl
-LNCR_BLD="21062500"
+LNCR_BLD="22020900"
 LNCR_ZIP="icons.zip"
 LNCR_NAME="CentOS"
 LNCR_FN=${LNCR_NAME}.exe
